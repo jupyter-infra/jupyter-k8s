c19c7b2308e6f73537251fda53abffbfe6f855d1
Sakshi Jain <jasakshi@amazon.com>
2025-10-31 20:26:02 +0000

    Reuse existing authz logic

--- internal/extensionapi/constants.go
+++ internal/extensionapi/constants.go
@@ -4,11 +4,6 @@ const (
 	// VSCodeScheme is the URL scheme for VSCode remote connections
 	VSCodeScheme = "vscode://amazonwebservices.aws-toolkit-vscode/connect/sagemaker"
 
-	// OwnershipTypePublic represents public workspace ownership
-	OwnershipTypePublic = "Public"
-	// OwnershipTypeOwnerOnly represents owner-only workspace ownership
-	OwnershipTypeOwnerOnly = "OwnerOnly"
-
 	// HeaderUser is the X-User header
 	HeaderUser = "X-User"
 	// HeaderRemoteUser is the X-Remote-User header
