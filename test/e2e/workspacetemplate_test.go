e9e86923a34c2748bd1ce3c336d77964cc5c6df1
Joshua Towner <josh@towner.xyz>
2025-11-03 14:11:02 -0800

    feat: add implementation for defaults for lifecycle, idle, access type, and app type

--- test/e2e/workspacetemplate_test.go
+++ test/e2e/workspacetemplate_test.go
@@ -558,5 +558,138 @@ spec:
 			Expect(err).NotTo(HaveOccurred())
 			Expect(output).To(BeEmpty())
 		})
+
+		It("should inherit access type and app type from template", func() {
+			By("creating template with default access type and app type")
+			templateYaml := `apiVersion: workspace.jupyter.org/v1alpha1
+kind: WorkspaceTemplate
+metadata:
+  name: access-app-type-template
+spec:
+  displayName: "Access and App Type Template"
+  defaultImage: "jupyter/base-notebook:latest"
+  defaultAccessType: "OwnerOnly"
+  appType: "jupyter-lab"
+  resourceBounds:
+    minCpu: "100m"
+    maxCpu: "2"
+    minMemory: "128Mi"
+    maxMemory: "4Gi"
+`
+			cmd := exec.Command("sh", "-c",
+				fmt.Sprintf("echo '%s' | kubectl apply -f -", templateYaml))
+			_, err := utils.Run(cmd)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("creating workspace without access type or app type")
+			workspaceYaml := `apiVersion: workspace.jupyter.org/v1alpha1
+kind: Workspace
+metadata:
+  name: access-app-type-test
+spec:
+  displayName: "Access App Type Test"
+  templateRef: "access-app-type-template"
+`
+			cmd = exec.Command("sh", "-c",
+				fmt.Sprintf("echo '%s' | kubectl apply -f -", workspaceYaml))
+			_, err = utils.Run(cmd)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("verifying workspace inherited access type")
+			Eventually(func(g Gomega) {
+				cmd := exec.Command("kubectl", "get", "workspace", "access-app-type-test",
+					"-o", "jsonpath={.spec.accessType}")
+				output, err := utils.Run(cmd)
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(output).To(Equal("OwnerOnly"))
+			}).WithTimeout(10 * time.Second).Should(Succeed())
+
+			By("verifying workspace inherited app type")
+			cmd = exec.Command("kubectl", "get", "workspace", "access-app-type-test",
+				"-o", "jsonpath={.spec.appType}")
+			output, err := utils.Run(cmd)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(output).To(Equal("jupyter-lab"))
+
+			By("cleaning up test resources")
+			cmd = exec.Command("kubectl", "delete", "workspace", "access-app-type-test", "--ignore-not-found")
+			_, _ = utils.Run(cmd)
+			cmd = exec.Command("kubectl", "delete", "workspacetemplate", "access-app-type-template", "--ignore-not-found")
+			_, _ = utils.Run(cmd)
+		})
+
+		It("should inherit lifecycle and idle shutdown from template", func() {
+			By("creating template with default lifecycle and idle shutdown")
+			templateYaml := `apiVersion: workspace.jupyter.org/v1alpha1
+kind: WorkspaceTemplate
+metadata:
+  name: lifecycle-template
+spec:
+  displayName: "Lifecycle Template"
+  defaultImage: "jupyter/base-notebook:latest"
+  defaultLifecycle:
+    postStart:
+      exec:
+        command: ["/bin/sh", "-c", "echo started"]
+  defaultIdleShutdown:
+    enabled: true
+    timeoutMinutes: 30
+    detection:
+      httpGet:
+        path: "/api/status"
+        port: 8888
+  resourceBounds:
+    minCpu: "100m"
+    maxCpu: "2"
+    minMemory: "128Mi"
+    maxMemory: "4Gi"
+`
+			cmd := exec.Command("sh", "-c",
+				fmt.Sprintf("echo '%s' | kubectl apply -f -", templateYaml))
+			_, err := utils.Run(cmd)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("creating workspace without lifecycle or idle shutdown")
+			workspaceYaml := `apiVersion: workspace.jupyter.org/v1alpha1
+kind: Workspace
+metadata:
+  name: lifecycle-test
+spec:
+  displayName: "Lifecycle Test"
+  templateRef: "lifecycle-template"
+`
+			cmd = exec.Command("sh", "-c",
+				fmt.Sprintf("echo '%s' | kubectl apply -f -", workspaceYaml))
+			_, err = utils.Run(cmd)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("verifying workspace inherited lifecycle")
+			Eventually(func(g Gomega) {
+				cmd := exec.Command("kubectl", "get", "workspace", "lifecycle-test",
+					"-o", "jsonpath={.spec.lifecycle.postStart.exec.command[0]}")
+				output, err := utils.Run(cmd)
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(output).To(Equal("/bin/sh"))
+			}).WithTimeout(10 * time.Second).Should(Succeed())
+
+			By("verifying workspace inherited idle shutdown")
+			cmd = exec.Command("kubectl", "get", "workspace", "lifecycle-test",
+				"-o", "jsonpath={.spec.idleShutdown.enabled}")
+			output, err := utils.Run(cmd)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(output).To(Equal("true"))
+
+			cmd = exec.Command("kubectl", "get", "workspace", "lifecycle-test",
+				"-o", "jsonpath={.spec.idleShutdown.timeoutMinutes}")
+			output, err = utils.Run(cmd)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(output).To(Equal("30"))
+
+			By("cleaning up test resources")
+			cmd = exec.Command("kubectl", "delete", "workspace", "lifecycle-test", "--ignore-not-found")
+			_, _ = utils.Run(cmd)
+			cmd = exec.Command("kubectl", "delete", "workspacetemplate", "lifecycle-template", "--ignore-not-found")
+			_, _ = utils.Run(cmd)
+		})
 	})
 })
