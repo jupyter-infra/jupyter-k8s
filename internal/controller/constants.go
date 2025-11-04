0aba87c187de81e55b538278cdcc43c2679a38c4
Henry Wang <henrywa@amazon.com>
2025-11-04 19:29:07 +0000

    Adding ServiceAccount defaulter

--- internal/controller/constants.go
+++ internal/controller/constants.go
@@ -27,6 +27,9 @@ const (
 	LabelAccessStrategyNamespace = "workspace.jupyter.org/accessStrategyNamespace"
 	LabelWorkspaceTemplate       = "workspace.jupyter.org/template"
 
+	// Default labels
+	LabelDefaultServiceAccount = "workspace.jupyter.org/default-service-account"
+
 	// Label values
 	AppLabelValue = "jupyter"
 
