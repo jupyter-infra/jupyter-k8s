9bad2d9bba24a3baa2146a0f2f80d340fff97747
Andrii Ieroshenko <ieroshenkoa@gmail.com>
2025-11-16 20:28:15 -0800

    move `TemplateViolation` struct to webhook package

--- internal/webhook/v1alpha1/validation_types.go
+++ internal/webhook/v1alpha1/validation_types.go
@@ -14,7 +14,7 @@ The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
 */
 
-package controller
+package v1alpha1
 
 // TemplateViolation describes a specific validation failure
 type TemplateViolation struct {
