5fa40349dd4afb4b7ef274381d73edb8954b16d9
Eilia Araghbidikashani <earaghbi@amazon.com>
2026-03-27 18:35:06 +0000

    fix linter

--- test/e2e/helpers.go
+++ test/e2e/helpers.go
@@ -232,10 +232,6 @@ func diagnoseAndCleanupStuckTemplates() {
 // createTemplateForTest creates a WorkspaceTemplate resource from a YAML file
 // filename: name of the YAML file (without .yaml extension)
 // group: primary directory (e.g., "template")
-// subgroup: optional subdirectory (e.g., "base" for "template-base/")
-// Note: group parameter currently always receives "template" but will be used for other groups in the future
-//
-//nolint:unparam
 // createNamespaceForTest creates a Namespace resource from a YAML file
 func createNamespaceForTest(filename, group, subgroup string) {
 	ginkgo.GinkgoHelper()
